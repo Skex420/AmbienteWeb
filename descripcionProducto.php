65475499f5c0341bdaece66d5540c0aad125cf2b
Jordi99CR <jlv291999@hotmail.com>
2021-07-30 17:45:13 -0600

    Boton de inicie sesion para invitado

--- descripcionProducto.php
+++ descripcionProducto.php
@@ -17,20 +17,22 @@
 
 <!DOCTYPE html>
 <html lang="en">
-    <head>
-        <meta charset="utf-8">
-        <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
-        <meta name="description" content="">
-        <meta name="author" content="">
-        <title>Descripción</title>
-        <link href="vendor/bootstrap/css/bootstrap.min.css" rel="stylesheet">
-        <link href="css/simple-sidebar.css" rel="stylesheet">
-        <link href="css/background.css" rel="stylesheet">
-        <link href="css/inicio.css" rel="stylesheet" type="text/css">
-    </head>
+
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <meta name="description" content="">
+    <meta name="author" content="">
+    <title>Descripción</title>
+    <link href="vendor/bootstrap/css/bootstrap.min.css" rel="stylesheet">
+    <link href="css/simple-sidebar.css" rel="stylesheet">
+    <link href="css/background.css" rel="stylesheet">
+    <link href="css/inicio.css" rel="stylesheet" type="text/css">
+</head>
 <?php
     include 'header.php'
 ?>
+
 <body>
     <div class="container-fluid">
         <div class="column-md-12">
@@ -41,25 +43,32 @@
     </div>
     <div class="container">
         <form action="" method="post">
-        <div class="row align-items-center">
-            <div class="col-6">
-                <?php
+            <div class="row align-items-center">
+                <div class="col-6">
+                    <?php
                     echo "<p class='h6'>Descripción: <br/>".str_replace('/','<br/><br/>',$productoEncontrado["DESCRIPCION"])."</p>";
                     echo "<p class='h4'><br/>Precio:<br/>₡".$productoEncontrado["VALOR"]."</p>";
                 ?>
-            </div>
-            <div class="col-6">
-                <?php
+                </div>
+                <div class="col-6">
+                    <?php
                     echo "<img class='img' src='" . $productoEncontrado["IMAGEN"] . "' height='100%'>";
                 ?>
+                </div>
             </div>
-        </div>
-        <div class="row">
-            <div class="column-md-auto">
-                <input type="submit" class="btn btn-dark border border-light" id="btnAgregarCarrito" name="btnAgregarCarrito" value="Agregar al carrito">
-            </div>
-        </div>
 
+            <div class="row">
+                <div class="column-md-auto">
+                    <?php
+                 if ($_SESSION['RolUsuario']==3){
+                    echo "<a href='logout.php' class='btn btn-dark border border-light'>Inicie sesión para comprar</a>";
+                }else{
+                    echo "<input type='submit' class='btn btn-dark border border-light' id='btnAgregarCarrito'
+                    name='btnAgregarCarrito' value='Agregar al carrito'>";
+                }
+                ?>
+                </div>
+            </div>
         </form>
     </div>
 </body>
